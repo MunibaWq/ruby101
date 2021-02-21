edfa0459e62824da1082642462a9fff90611afa8
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 18:31:08 -0700

    arrays: creating, accessing from beginning, accessing from end, modifying

---
+++ arrays.rb
@@ -0,0 +1,31 @@
+friends = Array["Kevin", "Karen", "Oscar"]
+
+# puts friends
+# OUTPUT:
+# Kevin
+# Karen
+# Oscar
+
+
+# puts friends[0]
+# OUTPUT:
+# Kevin
+
+
+# ACCESSING ARRAY ELEMENTS FROM THE BACK OF THE ARRAY:
+
+# puts friends[-1]
+#OUTPUT:
+# Oscar
+
+
+# ACCESSING A RANGE OF ELEMENTS FROM AN ARRAY:
+
+# puts friends[0,2]
+#OUTPUT:
+# Kevin
+# Karen
+
+# MODIFY ONE OF THE VALUES IN THE ARRAY:
+friends[0] = "Mike"
+puts friends[0]
