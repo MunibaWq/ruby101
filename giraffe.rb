c9825aebd8bd6ddb660a331c7f51cee1f0aebb20
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 17:52:03 -0700

    getting user input

---
+++ giraffe.rb
@@ -0,0 +1,33 @@
+# puts "    /|"
+# puts "   / |"
+# puts "  /  |"
+# puts " /   |"
+# puts "/____|"
+# num = 20
+# puts "\n"
+# puts ("my fav num " + num.to_s)
+# puts "\n"
+# num = -20.487
+# puts num.round()
+
+
+# user input in ruby - need to type "ruby giraffe.rb" in command line to get output
+
+# puts "Enter your name: "
+# name = gets
+# puts ("Hello " + name ) 
+
+# gets newline character when you do this"
+
+# puts "Enter your name: "
+# name = gets
+# puts ("Hello " + name + ", you are cool!" ) 
+# output: 
+# Hello m
+# , you are cool!
+
+#get rid of newline charater above by doing this:
+puts "Enter your name: "
+name = gets.chomp()
+puts ("Hello " + name + ", you are cool!" ) 
+# output: Hello m, you are cool!
