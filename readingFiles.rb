e27945f93e08648a8c00316be397b5043bb4d99b
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 23:46:17 -0700

    reading from an external text file

---
+++ readingFiles.rb
@@ -0,0 +1,33 @@
+
+# read a file
+File.open("word_set_1.txt", "r") do |file|
+puts file.read()
+end
+
+
+File.open("word_set_1.rb", "r") do |file|
+puts file.read().include? "Jim"
+end
+
+#readline - tells the file that we want to read lines individually, 
+puts file.readline()
+# returns an array of lines
+puts file.readline()[2]
+
+# reads individual characters in a file
+puts file.char()
+
+
+# loop through the array generated using readlines
+File.open("word.txt", "r") do |file|
+    for line in file.readlines()
+        puts line
+    end
+end
+
+
+# another way
+
+file = File.open("word.txt", "r")
+puts file.read
+file.close()
