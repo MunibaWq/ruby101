bbdba822c595dbe2af5810f1616e44ec92b16afa
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 20:30:26 -0700

    if - comparison

--- if.rb
+++ if.rb
@@ -43,3 +43,20 @@ istall = false
 
 #output:
 # You are a short male
+
+
+#COMPARISON:
+
+def max(num1, num2, num3)
+    if num1 >= num2 and num1 >= num3
+        return num1
+    elsif num2 >= num1 and num2 >= num3
+        return num2
+    else
+        return num3
+    # this is the end keyword for the if statement
+    end
+
+end
+
+puts max(1,2,3)
