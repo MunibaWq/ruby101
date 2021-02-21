d3826b4800dda417b63307a908a0458cb795fea9
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 20:19:34 -0700

    if else with and or

--- if.rb
+++ if.rb
@@ -1,9 +1,28 @@
 
-ismale = false
+ismale = true
 
-if ismale
- puts "You are male"
 
-else
-    puts "You are not male"
-end
+# if ismale
+#  puts "You are male"
+
+# else
+#     puts "You are not male"
+# end
+
+
+istall = false
+
+# if ismale and istall
+#  puts "You are a tall male"
+
+# else
+#     puts "You are either not male or not tall or both"
+# end
+
+
+if ismale or istall
+    puts "You are a tall male"
+   
+   else
+       puts "You are either not male or not tall or both"
+   end
