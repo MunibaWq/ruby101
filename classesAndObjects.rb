77420c51476cc3dda46b2582eb9278787e8e5cea
Muniba Waqar <muniba.wq@gmail.com>
2021-02-21 11:06:42 -0700

    initialize method

--- classesAndObjects.rb
+++ classesAndObjects.rb
@@ -23,18 +23,57 @@
 
 
 # Inititalize method - everytime when we create an object
-system "clear"
 
-class Square
-    def initialize(side_length)
-      @side_length = side_length
+# class Book
+#         attr_accessor :title, :author, :pages
+#         def initialize(name)
+#             puts ("hello #{name}")
+#         end
+#     end
+class Book
+        attr_accessor :title, :author, :pages
+        def initialize(title, author, pages)
+            @title = title
+            @author = author
+            @pages = pages
+         
+        end
     end
+
     
-    def side_length
-        return @side_length
-    end
-end
+    #object is an instance of a class
+    # book1 = Book.new("Mike" )
+    # book1.title = "Harry Potter"
+    # book1.author = "JK Rowling"
+    # book1.pages = 400
 
-my_square = Square.new(10)
-puts my_square.inspect
-puts my_square.side_length
+    book1 = Book.new("Harry Potter", "JK Rowling", 400 )
+  
+    
+    puts book1.pages
+    
+    
+    # book2 = Book.new("Bill")
+    # book2.title = "Lord of the Rings"
+    # book2.author = "Tolkein"
+    # book2.pages = 500
+
+
+
+
+
+# system "clear"
+
+# class Square
+#     def initialize(side_length)
+#       @side_length = side_length
+#     end
+    
+#     def side_length
+#         return @side_length
+#     end
+# end
+
+# my_square = Square.new(10)
+# puts my_square.inspect
+# puts my_square.side_length
