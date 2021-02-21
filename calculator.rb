bed9f181d19c7ba2c2c107839468fa87e377cec3
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 17:59:34 -0700

    adding numbers - concatenates issue

---
+++ calculator.rb
@@ -0,0 +1,13 @@
+# Building a calculator
+
+puts "Enter a number:"
+num1 = gets.chomp()
+puts "Enter another number:"
+num2 = gets.chomp()
+
+puts(num1 +num2)
+
+# num1 = 2, num2 = 2
+# output: 22
+
+
