a9ea4ab87a04dd75d26f3e8e154ba6a4c97bcd7f
Muniba Waqar <muniba.wq@gmail.com>
2021-02-21 10:21:17 -0700

    initialize method

--- classesAndObjects.rb
+++ classesAndObjects.rb
@@ -1,11 +1,11 @@
-# class = custom data type
+# # class = custom data type
 
 # class Book
 #     attr_accessor :title, :author, :pages
 # end
 
 
-#object is an instance of a class
+# #object is an instance of a class
 # book1 = Book.new()
 # book1.title = "Harry Potter"
 # book1.author = "JK Rowling"
@@ -23,25 +23,18 @@
 
 
 # Inititalize method - everytime when we create an object
+system "clear"
 
-class Book
-    @title @author @pages
-    def Inititalize(name)
-        puts "hello #{name}"
+class Square
+    def initialize(side_length)
+      @side_length = side_length
+    end
+    
+    def side_length
+        return @side_length
     end
 end
 
-book1 = Book.new(Mike)
-book1.title = "Harry Potter"
-book1.author = "JK Rowling"
-book1.pages = 400
-
-# puts book1.pages
-
-
-book2 = Book.new()
-book2.title = "Lord of the Rings"
-book2.author = "Tolkein"
-book2.pages = 500
-
-# puts book2.pages
+my_square = Square.new(10)
+puts my_square.inspect
+puts my_square.side_length
