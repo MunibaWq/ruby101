8a0b402bbf46756ac60d47a8167c051e2171b584
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 20:49:49 -0700

    while loops

---
+++ whileLoops.rb
@@ -0,0 +1,30 @@
+index = 1
+
+# a while loop keeps executing as long as its condition is true
+
+# while index <= 5
+#     puts index
+#     # index = index + 1
+#     index += 1
+    
+# end
+
+# OUTPUT:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+while index < 5
+    puts index
+    # index = index + 1
+    index += 1
+    
+end
+
+# OUTPUT:
+# 1
+# 2
+# 3
+# 4
