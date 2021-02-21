23a7e2936905404e6c2e5ba73c1c865a715273e1
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 19:33:50 -0700

    Hashes - 1

---
+++ hashes.rb
@@ -0,0 +1,29 @@
+# a type of data structure where we can store a bunch of information - similar to arrays
+# in hashes we can store key value pairs
+
+#build a hash that stores state code:
+# map a state to a specific abberviation
+
+# states = {
+#     "Pennsylvania" => "PA",
+#     "New York" => "NY",
+#     "Oregon" => "OR"
+# }
+
+# puts states
+#OUTPUT:
+# {"Pennsylvania"=>"PA", "New York"=>"NY", "Oregon"=>"OR"}
+
+# puts states["Oregon"]
+#OUTPUT
+# OR
+
+states = {
+    :Pennsylvania => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states[:Pennsylvania]
+#OUTPUT
+# PA
