23c81056f3680f44380458643274a08d2ca5be7e
Muniba Waqar <muniba.wq@gmail.com>
2021-02-20 20:58:28 -0700

    game with if/else/while loops

--- guessGame.rb
+++ guessGame.rb
@@ -1,9 +1,31 @@
+# secret_word = "giraffe"
+# guess = ""
+
+# while guess != secret_word
+#     puts "Enter your guess:"
+#     guess = gets.chomp()
+# end
+
+# puts "You Won!"
+
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
-while guess != secret_word
-    puts "Enter your guess:"
-    guess = gets.chomp()
+while guess != secret_word and !out_of_guesses
+    if guess_count < guess_limit
+        puts "Enter your guess:"
+        guess = gets.chomp()
+        guess_count +=1
+    else
+        out_of_guesses = true
+    end
 end
 
-puts "You Won!"
+if out_of_guesses
+    puts "You lose!"
+else
+    puts "You Won!"
+end
